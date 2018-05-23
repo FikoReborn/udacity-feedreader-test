b008eb948bdf2bdd75a2ac2aa6c12b219d4b3275
Adam M Duff <fikosan@gmail.com>
2018-05-23 16:09:07 -0400

    refactor: Tests added to check menu visibility

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,20 +52,28 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* This is a test suite to check menu visibility */
+    describe('The menu', function() {
+        /* Test to check if menu is hidden by default when the page loads */
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
+         /* This is a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+         it('is hidden when clicked', function() {
+             // Click the menu icon then see if the body tag does not have class 'menu-hidden'
+             $('.menu-icon-link').trigger('click');
+             expect($('body').hasClass('menu-hidden')).not.toBe(true);
+             // Click the menu icon again, then see if the body tag DOES have class 'menu-hidden'
+             $('.menu-icon-link').trigger('click');
+             expect($('body').hasClass('menu-hidden')).toBe(true);
+             
+         });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
