52498cbe39420c423177542eb8a6e44c95fca8f6
Adam M Duff <fikosan@gmail.com>
2018-05-24 08:50:13 -0400

    refactor: Test for initial entries added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -74,14 +74,28 @@ $(function() {
              
          });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Testing "Initial Entries" */
+    describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This is a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        //  First, let's make sure the function runs before the test
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        // Now let's make sure there are more than 0 ".entry" elements within the ".feed" element
+        it('is not empty', function() {
+            expect($('.feed').find('.entry').length > 0).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
